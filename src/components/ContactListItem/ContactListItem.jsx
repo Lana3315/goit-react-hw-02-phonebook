c1256bd58d17e55abbcbd8fc022050efbed662f2
Lana3315 <serookayalana@gmail.com>
2023-07-01 15:49:34 +0200

    update

---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+export const ContactsListItem = ({ id, name, number, deleteContact }) => {
+  return (
+    <li key={id}>
+      <p>
+        {name}:<span>{number}</span>
+      </p>
+      <button onClick={() => deleteContact(id)}>Delete</button>
+    </li>
+  );
+};
+
+ContactsListItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactsListItem;
